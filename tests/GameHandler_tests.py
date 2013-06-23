d3044d1e56ef86b269f0546f5d4b369fcd7afad0
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-06-23 15:24:39 -0600

    Adding Tests

--- tests/GameHandler_tests.py
+++ tests/GameHandler_tests.py
@@ -4,16 +4,57 @@ from GameHandler import GameHandler
 class TestInit(unittest.TestCase):
     
     def setUp(self):
-        self.gameHandler = GameHandler(8, 1000)
+        self.gameHandler = GameHandler(8, 1000, True)
 
     def test_opponents(self):
-        self.assertEqual(self.gameHandler.opponents, 8)
+        self.assertEqual(self.gameHandler.opponents, 7)
         
 class TestHeadsUp(unittest.TestCase):
     
     def setUp(self):
-        self.gameHandler = GameHandler(1, 1000)
+        self.gameHandler = GameHandler(2, 1000, True)
         
+    def testDealer(self):
+        self.assertEqual(self.gameHandler.players[0].getDealerStatus(), True)
+        
+    def testPointerAfterBlinds(self):
+        self.gameHandler.postBlinds()
+        self.gameHandler.advancePointer()
+        self.assertEqual(self.gameHandler.pointer, 1)
+        
+    def testActiveStatusAfterBlinds(self):
+        self.gameHandler.postBlinds()
+        self.assertEqual(self.gameHandler.players[0].getActiveStatus(), True)
+        
+    def testChangeActionPlayer(self):
+        self.gameHandler.postBlinds()
+        self.gameHandler.changeActionPlayer()
+        self.assertEqual(self.gameHandler.players[1].getActiveStatus(), True)
+        
+class TestRingGame(unittest.TestCase):
+    
+    def setUp(self):
+        self.gameHandler = GameHandler(8, 1000, True)
+        
+    def testDealer(self):
+        self.assertEqual(self.gameHandler.players[0].getDealerStatus(), True)
+        
+    def testPointer(self):
+        self.assertEqual(self.gameHandler.pointer, 1)
+    
+    def testPointerAfterBlinds(self):
+        self.gameHandler.postBlinds()
+        self.gameHandler.advancePointer()
+        self.assertEqual(self.gameHandler.pointer, 4)
+    
+    def testActiveStatusAfterBlinds(self):
+        self.gameHandler.postBlinds()
+        self.assertEqual(self.gameHandler.players[3].getActiveStatus(), True)
+        
+    def testChangeActionPlayer(self):
+        self.gameHandler.postBlinds()
+        self.gameHandler.changeActionPlayer()
+        self.assertEqual(self.gameHandler.players[4].getActiveStatus(), True)
         
 if __name__ == '__main__':
     unittest.main()
